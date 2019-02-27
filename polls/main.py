e299697095a10983a4f2e3ee6c8b3c5db373a2d3
Eugene <Fl1p92@ukr.net>
2019-02-27 16:45:14 +0200

    Add database settings and templates.

--- polls/main.py
+++ polls/main.py
@@ -1,9 +1,21 @@
+import aiohttp_jinja2
+import jinja2
 from aiohttp import web
 
-from routes import setup_routes
-
+from config.settings import config
+from polls.models import init_pg, close_pg
+from polls.routes import setup_routes
 
 app = web.Application()
 setup_routes(app)
+app['config'] = config
+
+# setup Jinja2 template renderer
+aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('polls', 'templates'))
+
+# signals for db connection on start and close on shutdown
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
+
 
 web.run_app(app)
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,18 @@
+import aiohttp_jinja2
 from aiohttp import web
 
+from polls import models
 
-async def index(request):
-    return web.Response(text='Hello Aiohttp!')
+
+@aiohttp_jinja2.template('detail.html')
+async def poll(request):
+    async with request.app['db'].acquire() as conn:
+        question_id = request.match_info['question_id']
+        try:
+            question, choices = await models.get_question(conn, question_id)
+        except models.RecordNotFound as e:
+            raise web.HTTPNotFound(text=str(e))
+        return {
+            'question': question,
+            'choices': choices
+        }
